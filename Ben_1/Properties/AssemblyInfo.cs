f27fe5b729881ad080e875498de7685031132d1f
Sathish <Sathish@BBSS-L-201>
2023-06-05 14:54:41 +0800

    Add project files.

---
+++ Ben_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Ben_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("Ben_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("D246A246-4C92-4F2F-9AF1-61C412D690D9")]
+[assembly: AssemblyVersion("1.0.0.0")]
